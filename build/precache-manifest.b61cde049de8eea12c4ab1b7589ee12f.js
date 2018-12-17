8426797c0ded2184785eb5cc3b74171e3e48b838
Cu Le Huy <lcu@usc.edu>
2018-12-17 10:42:00 +0100

    initial commit

---
+++ build/precache-manifest.b61cde049de8eea12c4ab1b7589ee12f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0c7018fe88d8f375a662",
+    "url": "/static/js/main.0c7018fe.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "0c7018fe88d8f375a662",
+    "url": "/static/css/main.dc1067c8.chunk.css"
+  },
+  {
+    "revision": "6daddb29b0b2bdf63c44c9b8683fab84",
+    "url": "/index.html"
+  }
+];
